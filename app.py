a152f327b4066ec0337f9ad348fd8f021a3a1d64
Eduardo Molina <eduardo.molina@live.com>
2018-02-23 23:55:35 -0300

    Remove logs

--- app.py
+++ app.py
@@ -8,13 +8,6 @@ def main(config):
     summoner = Summoner(summoner_name, config['api_secret_key'])
     champions = Champions(config['api_secret_key'])
 
-    print('{}\n{}\n{}\n{}\n{}\n{}'.format(summoner.name,
-                                          summoner.account_id,
-                                          summoner.summoner_id,
-                                          summoner.roles,
-                                          summoner.champions_mastery,
-                                          champions.get_all_champions_info()))
-
 
 if __name__ == '__main__':
     config = to_dict('config/riot_dev_api_key.json')
